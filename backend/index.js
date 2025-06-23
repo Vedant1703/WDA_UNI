0946eb7594c1ad25ab7753c5ec573b77b3a64668
Vedant <vedantkulkarni@vedants-MacBook-Air.local>
2025-06-23 16:41:47 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,10 @@ dotenv.config();
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
-
+app.use(cors({
+  origin: 'https://wda-uni-yd54.vercel.app',  // your Vercel frontend URL
+  credentials: true // only if you're using cookies, sessions, or auth
+}));
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
